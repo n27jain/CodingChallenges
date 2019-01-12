1e146e352df3c872f2a5d72b176c0d10731ef338
Master Hacker <n27jain@edu.uwaterloo.ca>
2019-01-12 01:00:20 -0500

    Able to get json string now

--- Shopify Project/app/src/main/java/me/namankumarjain/customcollections/CategoriesPage.kt
+++ Shopify Project/app/src/main/java/me/namankumarjain/customcollections/CategoriesPage.kt
@@ -1,29 +1,46 @@
 package me.namankumarjain.customcollections
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_categories_page.*
+import okhttp3.*
+import java.io.IOException
 
 
 class CategoriesPage : AppCompatActivity() {
 
 
     private var client = OkHttpClient()
-    //private val textView: TextView = findViewById(R.id.specialBox)
+    var TAG = "DebugMessage"
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories_page)
-        val x = run("https://shopicruit.myshopify.com/admin/collects.json?collection_id=68424466488&page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6")
-        println(message = x)
+        val text = retrieveJson("https://shopicruit.myshopify.com/admin/custom_collections.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6")
+
+        specialBox.text = text.toString()
     }
 
-    private fun run(url: String): String? {
+    private fun retrieveJson(url: String): String? {
         val request = Request.Builder()
                 .url(url)
                 .build()
+        var body:String? = null
+        client.newCall(request).enqueue(object : Callback{
+            override fun onResponse(call: Call, response: Response?) {
+                body = response?.body()?.string()
+                Log.d(TAG, body)
+            }
 
-        client.newCall(request).execute().use { response -> return response.body()?.string() }
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d(TAG, "Failure")
+                body = null
+            }
+
+        })
+        return body
     }
 
 }
