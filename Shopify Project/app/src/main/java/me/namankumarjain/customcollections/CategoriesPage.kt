66ec29becaf7e11a0cb1579bbe42357315997214
Master Hacker <n27jain@edu.uwaterloo.ca>
2019-01-11 23:21:58 -0500

    Update Android Studio

--- Shopify Project/app/src/main/java/me/namankumarjain/customcollections/CategoriesPage.kt
+++ Shopify Project/app/src/main/java/me/namankumarjain/customcollections/CategoriesPage.kt
@@ -1,8 +1,6 @@
 package me.namankumarjain.customcollections
-
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
@@ -11,17 +9,16 @@ class CategoriesPage : AppCompatActivity() {
 
 
     private var client = OkHttpClient()
-    val textView: TextView = findViewById(R.id.textbox_01)
+    //private val textView: TextView = findViewById(R.id.specialBox)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories_page)
-        val jsonString = run("https://shopicruit.myshopify.com/admin/custom_collections.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6")
-        if(jsonString != null)
-            textView.text = jsonString
+        val x = run("https://shopicruit.myshopify.com/admin/collects.json?collection_id=68424466488&page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6")
+        println(message = x)
     }
 
-    fun run(url: String): String? {
+    private fun run(url: String): String? {
         val request = Request.Builder()
                 .url(url)
                 .build()
